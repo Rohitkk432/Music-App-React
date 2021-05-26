22b01e9caa6dcd4df04d0195fcae83cbf6e3fa0e
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2021-05-26 19:16:16 +0530

    attempt to convert login file to class instead of func

--- src/components/login_page_components/googlelogin.js
+++ src/components/login_page_components/googlelogin.js
@@ -1,48 +1,96 @@
-import React from 'react';
+import { React,Component } from 'react';
 import './googlelogin.css';
 import { GoogleLogin } from 'react-google-login';
 import {useHistory} from 'react-router-dom';
 
 var currentUser;
 var currentId;
-function Login () {
-        let history = useHistory();
-        const onSuccess = (res)=>{
+class Login extends Component{
+        // let history = useHistory();
+        // const onSuccess = (res)=>{
+        //     currentUser = res.profileObj.email;
+        //     checkOfUser();
+        //     // console.log(currentUser);
+        //     history.push('/home');
+        // }
+        // const onFailure =(res)=>{
+        //     console.log(res);
+        // }
+        
+        // // 
+        // async function checkOfUser(){
+        //     await fetch(`https://music-pro-x-server.herokuapp.com/users/${currentUser}`,{ method: "GET"})
+        //     .then((resp)=>resp.json())
+        //     .then((res)=>{
+        //         if(res){
+        //             currentId=res.id;
+        //         }else{
+        //             fetch('https://music-pro-x-server.herokuapp.com/users',{
+        //                 method:"POST",
+        //                 headers:{"Content-Type": "application/json"},
+        //                 body: JSON.stringify({"email":`${currentUser}`})
+        //             })
+        //             .then((res)=>res.json())
+        //             .then((user)=>{
+        //                 currentId=user.id
+        //             })
+        //             .catch((err)=>console.log(err));
+        //         }
+        //     })
+        // }
+    constructor(){
+        super();
+        this.state = {onSuccess:function(res){
             currentUser = res.profileObj.email;
-            checkOfUser();
+            this.state.checkOfUser();
             // console.log(currentUser);
-            history.push('/home');
-        }
-        const onFailure =(res)=>{
+            useHistory.push('/home');
+        },onFailure:function(res){
             console.log(res);
-        }
-        
-        // 
-        async function checkOfUser(){
-            await fetch(`https://music-pro-x-server.herokuapp.com/users/${currentUser}`,{ method: "GET"})
-            .then((resp)=>resp.json())
-            .then((res)=>{
-                if(res){
-                    currentId=res.id;
-                }else{
-                    fetch('https://music-pro-x-server.herokuapp.com/users',{
-                        method:"POST",
-                        headers:{"Content-Type": "application/json"},
-                        body: JSON.stringify({"email":`${currentUser}`})
-                    })
-                    .then((res)=>res.json())
-                    .then((user)=>{
-                        currentId=user.id
-                    })
-                    .catch((err)=>console.log(err));
-                }
-            })
-        }
+        },checkOfUser:async function(){
+        }};
+    }
 
-        
+    async componentDidMount() {
+        // let history = useHistory();
+        // const onSuccess = (res)=>{
+        //     currentUser = res.profileObj.email;
+        //     checkOfUser();
+        //     // console.log(currentUser);
+        //     history.push('/home');
+        // }
+        // const onFailure =(res)=>{
+        //     console.log(res);
+        // }
 
+        // this.setState({history:{useHistory});
+        
+        this.setState({checkOfUser(){
+                fetch(`https://music-pro-x-server.herokuapp.com/users/${currentUser}`,{ method: "GET"})
+                .then((resp)=>resp.json())
+                .then((res)=>{
+                    if(res){
+                        currentId=res.id;
+                    }else{
+                        fetch('https://music-pro-x-server.herokuapp.com/users',{
+                            method:"POST",
+                            headers:{"Content-Type": "application/json"},
+                            body: JSON.stringify({"email":`${currentUser}`})
+                        })
+                        .then((res)=>res.json())
+                        .then((user)=>{
+                            currentId=user.id
+                        })
+                        .catch((err)=>console.log(err));
+                    }
+                })
+            }
+        })
+    }
+        
+    render(){
         return(
-            <div className="login-side">
+                <div className="login-side">
                     <div className="apptext">
                         <div className="app-name">Music Pro X</div>
                         <div className="makers-org">A DevSoc Project</div>
@@ -53,19 +101,20 @@ function Login () {
                             <div className="bits-disclaimer">BITS mail</div>
                         </div>
                         <GoogleLogin
-                            clientId="132873793273-jga64pb0l5nd25g2kjsr8rqm10tpt9pe.apps.googleusercontent.com"
+                            clientId="132873793273-jga64pb0l5nd25g2kjsr8rqm10tpt9pe.apps.  googleusercontent.com"
                             buttonText="Login with Google"
                             prompt="select_account"
                             uxMode="popup"
                             redirectUri="https://musicprox.netlify.app/home"
                             isSignedIn={true}
-                            onSuccess={onSuccess}
-                            onFailure={onFailure}
+                            onSuccess={this.state.onSuccess}
+                            onFailure={this.state.onFailure}
                             cookiePolicy={'single_host_origin'}
                         />                
                     </div>
                 </div>
         )    
+    }
 }
 export {currentUser,currentId};
 export default Login;
